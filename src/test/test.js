7d22e6657a3bc9f14bcd03560b68fbea3d15c585
Artem Maksimov <temamaksimov@gmail.com>
2017-09-08 23:50:27 +0300

    Fix feedback from test

--- src/test/test.js
+++ src/test/test.js
@@ -11,8 +11,10 @@ describe('Testing of all sort algorithms', function () {
     describe(`testing ${functionName}` , function () {
 
       describe('Large Array (1000 items) with random elements from 0 to 9999', function () {
-        const largeArray = new Array(1000).fill(0).map(() => getRandomInt(0, 1000));
-        expect(largeArray.sort((a, b) => { return a - b })).to.deep.equal(sortFunction(largeArray))
+        it(`should sort array`, function () {
+          const largeArray = new Array(1000).fill(0).map(() => getRandomInt(0, 1000));
+          expect(largeArray.sort((a, b) => { return a - b })).to.deep.equal(sortFunction(largeArray))
+        })
       })
 
     })
