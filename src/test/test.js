d9c6cc52abaac94716e57ff8a926184197cecd4e
Artem Maksimov <temamaksimov@gmail.com>
2017-09-10 00:14:41 +0300

    Improve tests

--- src/test/test.js
+++ src/test/test.js
@@ -5,16 +5,38 @@ import { getRandomInt } from '../utils/random'
 
 const listOfSortFunctions = [nativeSort]
 
+const checkSortFunction = (sortFunction, array) => {
+  expect(array.sort((a, b) => { return a - b })).to.deep.equal(sortFunction(array))
+}
+
 describe('Testing of all sort algorithms', function () {
   listOfSortFunctions.forEach((sortFunction) => {
     const functionName = sortFunction.name; 
     describe(`testing ${functionName}` , function () {
 
+      it(`should sort empty array`, function () {
+        checkSortFunction(sortFunction, [])
+      })
+
+      it(`should sort array (1 element)`, function () {
+        checkSortFunction(sortFunction, [1])
+      })
+
+      it(`should sort array (2 ordered elements)`, function () {
+        checkSortFunction(sortFunction, [1, 2])
+      })
+
+      it(`should sort array (2 unordered elements)`, function () {
+        checkSortFunction(sortFunction, [2, 1])
+      })
+
       describe('Large Array (1000 items) with random elements from 0 to 9999', function () {
-        it(`should sort array`, function () {
-          const largeArray = new Array(1000).fill(0).map(() => getRandomInt(0, 1000));
-          expect(largeArray.sort((a, b) => { return a - b })).to.deep.equal(sortFunction(largeArray))
-        })
+        for(let i = 0; i < 100; i++) {
+          it(`should sort array #${i}`, function () {
+            const largeArray = new Array(1000).fill(0).map(() => getRandomInt(0, 1000));
+            checkSortFunction(sortFunction, largeArray)
+          })
+        }
       })
 
     })
