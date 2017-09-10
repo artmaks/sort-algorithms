523c3e141d8bfc3e2281d6e3327d0275a70f7ff6
Artem Maksimov <temamaksimov@gmail.com>
2017-09-10 20:53:03 +0300

    Add test for demonstrating shell sort advantages over simple insertion sort

--- src/test/test.js
+++ src/test/test.js
@@ -3,16 +3,21 @@ import { expect } from 'chai';
 import { nativeSort } from '../algorithms/native'
 import { bubbleSort, bubbleSortWithAdvancedSwap, bubbleSortWithFlag } from '../algorithms/bubble'
 import { selectionSort } from '../algorithms/selection'
-import { insertionSort } from '../algorithms/insertion'
+import { insertionSort, shellSort } from '../algorithms/insertion'
 import { getRandomInt } from '../utils/random'
 
-const listOfSortFunctions = [nativeSort, bubbleSort, bubbleSortWithAdvancedSwap, bubbleSortWithFlag, selectionSort, insertionSort]
+const listOfSortFunctions = [nativeSort, bubbleSort, bubbleSortWithAdvancedSwap, bubbleSortWithFlag, 
+  selectionSort, insertionSort, shellSort ]
 
 const checkSortFunction = (sortFunction, array) => {
   const arr1 = array.slice(0);
   const arr2 = array.slice(0);
   const answer = arr1.sort((a, b) => { return a - b });
   expect(answer).to.deep.equal(sortFunction(arr2))
+}
+
+const isSlowSortFunction = (sortFunction) => {
+  return !['nativeSort', 'insertionSort', 'shellSort'].includes(sortFunction.name)
 }
 
 describe('Testing of all sort algorithms', function () {
@@ -36,14 +41,24 @@ describe('Testing of all sort algorithms', function () {
         checkSortFunction(sortFunction, [2, 1])
       })
 
-      // describe('Large Array (1000 items) with random elements from 0 to 9999', function () {
-      //   for(let i = 0; i < 100; i++) {
-      //     it(`should sort array #${i}`, function () {
-      //       const largeArray = new Array(1000).fill(0).map(() => getRandomInt(0, 1000));
-      //       checkSortFunction(sortFunction, largeArray)
-      //     })
-      //   }
-      // })
+      describe('Large Array (1.000 items) with random elements from 0 to 9999', function () {
+        for(let i = 0; i < 100; i++) {
+          it(`should sort array #${i}`, function () {
+            const largeArray = new Array(1000).fill(0).map(() => getRandomInt(0, 1000));
+            checkSortFunction(sortFunction, largeArray)
+          })
+        }
+      })
+
+      describe('Very large Array (50.000 items) with random elements from 0 to 9999', function () {
+        for(let i = 0; i < 10; i++) {
+          it(`should sort array #${i}`, function () {
+            if(isSlowSortFunction(sortFunction)) this.skip();
+            const largeArray = new Array(50000).fill(0).map(() => getRandomInt(0, 1000));
+            checkSortFunction(sortFunction, largeArray)
+          })
+        }
+      })
 
     })
   })
