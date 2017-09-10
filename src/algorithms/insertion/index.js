6d7a7fd9f8f7eb6e505cf8d8e498923d22acf1bd
Artem Maksimov <temamaksimov@gmail.com>
2017-09-10 20:52:19 +0300

    Add shell sort

--- src/algorithms/insertion/index.js
+++ src/algorithms/insertion/index.js
@@ -1,6 +1,6 @@
 function insertionSort(array) {
   for(let i = 0; i < array.length; i++) {
-    let element = array[i]
+    const element = array[i]
     let index = i - 1
 
     while(index >= 0 && array[index] > element) {
@@ -10,10 +10,35 @@ function insertionSort(array) {
 
     array[index + 1] = element
   }
+
+  return array
+}
+
+function shellSort(array) {
+  const len = array.length
+  let step = parseInt(len / 2)
+
+  while(step > 0) {
+
+    for (let i = step; i < len; i++) {
+      const element = array[i]
+      let index = i - step
+
+      while (index >= 0 && array[index] > element) {
+        array[index + step] = array[index]
+        index -= step
+      }
+
+      array[index + step] = element
+    }
+
+    step = parseInt(step / 2)
+  }
   
   return array
 }
 
 export {
   insertionSort,
+  shellSort,
 }
